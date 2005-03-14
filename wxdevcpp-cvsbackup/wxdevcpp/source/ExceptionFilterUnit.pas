bdc45c9d3c53f7c5303ac4d0c9184e5f06bc4f1e
gururamnath <gururamnath@917957a4-63a1-4874-a357-9ccf276ff571>
2005-03-14 02:35:20 +0000

    Support for additional user made exception display
    git-svn-id: svn://svn.code.sf.net/p/wxdsgn/code/trunk@84 917957a4-63a1-4874-a357-9ccf276ff571

--- wxdevcpp-cvsbackup/wxdevcpp/source/ExceptionFilterUnit.pas
+++ wxdevcpp-cvsbackup/wxdevcpp/source/ExceptionFilterUnit.pas
@@ -4,7 +4,7 @@ interface
 
 implementation
 
-uses Classes,windows,madExcept,JvInspector,ELDsgnr;
+uses Classes,windows,madExcept,JvInspector,ELDsgnr,sysutils;
 
 procedure ExceptionFilter(frozen          : boolean;
                           exceptObject    : TObject;
@@ -17,21 +17,25 @@ procedure ExceptionFilter(frozen          : boolean;
 begin
   //This is for People who try to enter caption or Label information in the Name Property
   if (exceptObject <> nil) and (exceptObject is EComponentError) then begin
-    MessageBox(0, pchar(EComponentError(exceptObject).Message), 'Error...', MB_ICONERROR or MB_TASKMODAL);
+    MessageBox(0, pchar(EComponentError(exceptObject).Message +#13+#10+#13+#10+'After closing this message Box, press escape to resume.'), 'Error...', MB_ICONERROR or MB_TASKMODAL);
     handled := true;
   end;
   //This is for screwing up some Property Inspector Editor specifics like Entering Data
   // in the Orientation Information
   if (exceptObject <> nil) and (exceptObject is EJvInspectorItem) then begin
-    MessageBox(0, pchar(EJvInspectorItem(exceptObject).Message), 'Error...', MB_ICONERROR or MB_TASKMODAL);
+    MessageBox(0, pchar(EJvInspectorItem(exceptObject).Message+#13+#10+#13+#10+'After closing this message Box, press escape to resume.'), 'Error...', MB_ICONERROR or MB_TASKMODAL);
     handled := true;
   end;
   //For some Weird Designer Errors.
   if (exceptObject <> nil) and (exceptObject is EELDesigner) then begin
-    MessageBox(0, pchar(EELDesigner(exceptObject).Message), 'Error...', MB_ICONERROR or MB_TASKMODAL);
+    MessageBox(0, pchar(EELDesigner(exceptObject).Message+#13+#10+#13+#10+'After closing this message Box, press escape to resume.'), 'Error...', MB_ICONERROR or MB_TASKMODAL);
     handled := true;
   end;
-
+  //If user accidently entered text to a Integer field
+  if (exceptObject <> nil) and (exceptObject is EConvertError) then begin
+    MessageBox(0, pchar(EConvertError(exceptObject).Message+#13+#10+#13+#10+'After closing this message Box, press escape to resume.'), 'Error...', MB_ICONERROR or MB_TASKMODAL);
+    handled := true;
+  end;
 end;
 
 initialization
