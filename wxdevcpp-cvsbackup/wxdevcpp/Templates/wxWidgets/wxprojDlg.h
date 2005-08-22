e65fb6703f407fa531dea3e11ab0fcba512a8015
lowjoel <lowjoel@917957a4-63a1-4874-a357-9ccf276ff571>
2005-08-22 09:56:47 +0000

    Fixed some code indentations (trivial) as well comments (and their spelling errors)
    git-svn-id: svn://svn.code.sf.net/p/wxdsgn/code/trunk@147 917957a4-63a1-4874-a357-9ccf276ff571

--- wxdevcpp-cvsbackup/wxdevcpp/Templates/wxWidgets/wxprojDlg.h
+++ wxdevcpp-cvsbackup/wxdevcpp/Templates/wxWidgets/wxprojDlg.h
@@ -3,65 +3,71 @@
 // Name:        %FILE_NAME%.h
 // Author:      %AUTHOR_NAME%
 // Created:     %DATE_STRING%
+// Description: 
 //
 //---------------------------------------------------------------------------
-#ifndef __%CLASS_NAME%_HPP_
-#define __%CLASS_NAME%_HPP_
 
-#include <wx/wxprec.h>
+#ifndef __%CAP_CLASS_NAME%_h__
+#define __%CAP_CLASS_NAME%_h__
+
 #ifdef __BORLANDC__
-        #pragma hdrstop
-#endif
-#ifndef WX_PRECOMP
-        #include <wx/wx.h>
+	#pragma hdrstop
 #endif
 
-//Do not add custom headers.
-//wx-dvcpp designer will remove them
+#ifndef WX_PRECOMP
+	#include <wx/wx.h>
+	#include <wx/dialog.h>
+#else
+	#include <wx/wxprec.h>
+#endif
+
+//Do not add custom headers
+//wxDev-C++ designer will remove them
 ////Header Include Start
 ////Header Include End
 
-#include <wx/dialog.h>
+//Compatibility for 2.4 code
+#ifndef wxCLOSE_BOX
+	#define wxCLOSE_BOX 0x1000
+#endif
+#ifndef wxFIXED_MINSIZE
+	#define wxFIXED_MINSIZE 0
+#endif
 
 ////Dialog Style Start
-#define %CLASS_NAME%_STYLE  %CLASS_STYLE_STRING%
+#define %CLASS_NAME%_STYLE %CLASS_STYLE_STRING%
 ////Dialog Style End
 
 class %CLASS_NAME% : public wxDialog
 {
-public:
-    %CLASS_NAME%( wxWindow *parent, wxWindowID id = 1, const wxString &title = wxT("%CLASS_TITLE%1"),
-        const wxPoint& pos = wxDefaultPosition,
-        const wxSize& size = wxDefaultSize,
-        long style = %CLASS_NAME%_STYLE);
-    virtual ~%CLASS_NAME%();
-public:
-  //Do not add custom Control Declarations here.
-  //wx-devcpp will remove them. Try adding the custom code 
-  //after the block.
-  ////GUI Control Declaration Start
-  ////GUI Control Declaration End
-public:
-
-    //Note: if you receive any error with these enums, then you need to
-    //change your old form code that are based on the #define control ids.
-    //#defines may replace a numeric value for the enums names.
-    //Try copy pasting the below block in your old Form header Files.
-	enum {
-////GUI Enum Control ID Start
-////GUI Enum Control ID End
-   ID_DUMMY_VALUE_ //Dont Delete this DummyValue
-   }; //End of Enum
-private:
-    DECLARE_EVENT_TABLE()
-
-public:
-    void %CLASS_NAME%Close(wxCloseEvent& event);
-    void CreateGUIControls(void);
+	private:
+		DECLARE_EVENT_TABLE();
+		
+	public:
+		%CLASS_NAME%(wxWindow *parent, wxWindowID id = 1, const wxString &title = wxT("%CLASS_TITLE%"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = %CLASS_NAME%_STYLE);
+		virtual ~%CLASS_NAME%();
+	
+	public:
+		//Do not add custom control declarations
+		//wxDev-C++ will remove them. Add custom code after the block.
+		////GUI Control Declaration Start
+		////GUI Control Declaration End
+		
+	public:
+		//Note: if you receive any error with these enum IDs, then you need to
+		//change your old form code that are based on the #define control IDs.
+		//#defines may replace a numeric value for the enum names.
+		//Try copy and pasting the below block in your old form header files.
+		enum
+		{
+			////GUI Enum Control ID Start
+			////GUI Enum Control ID End
+			ID_DUMMY_VALUE_ //don't remove this value unless you have other enum values
+		}; //End of Enum
+	
+	public:
+		void %CLASS_NAME%Close(wxCloseEvent& event);
+		void CreateGUIControls(void);
 };
 
 #endif
- 
- 
- 
- 
