0b1a28053bb5d52918015c2aa0d48f3585d6675c
lowjoel <lowjoel@917957a4-63a1-4874-a357-9ccf276ff571>
2006-09-27 09:00:50 +0000

    Indentation and minor code clean ups
    git-svn-id: svn://svn.code.sf.net/p/wxdsgn/code/trunk@449 917957a4-63a1-4874-a357-9ccf276ff571

--- wxdevcpp-cvsbackup/wxdevcpp/source/Search_Center.pas
+++ wxdevcpp-cvsbackup/wxdevcpp/source/Search_Center.pas
@@ -21,22 +21,6 @@ unit Search_Center;
 
 interface
 
-{
-  mandrav 6 Aug 2002:
-  -------------------
-  Newest SynEdit needs TSynEditSearch class to do the search. This class does not
-  exist in older versions of SynEdit (namely the one used in dev).
-  The temporary workaround, until we incorporate the latest SynEdit in the dev
-  CVS tree, is the next define, which by default will be disabled so that anyone
-  wanting to build dev from sources be able to...
-  I will enable this define only to build a new exe and then disable it again
-  (I am using the latest SynEdit here ;).
-  The only known problem without this define is that any kind of search inside
-  dev will fail (if one uses the latest SynEdit of course)...
-  If only SynEdit had a define with its version in it...
-}
-
-
 uses
 {$IFDEF WIN32}
  Classes, Types, Project, Editor, utils, SynEdit, ComCtrls,
@@ -115,32 +99,32 @@ begin
   AssignSearchEngine;
   if fReplace then
   begin
-     frmReplace.cboFindText.Text:= fFindText;
-     return:= frmReplace.ShowModal;
+    frmReplace.cboFindText.Text:= fFindText;
+    return := frmReplace.ShowModal;
     if (return = mrOk) or (return = mrAll) then
     begin
-        fFindText:= frmReplace.cboFindText.Text;
-        fReplaceText:= frmReplace.cboReplaceText.Text;
-        fOptions:= frmReplace.SearchOptions;
+      fFindText:= frmReplace.cboFindText.Text;
+      fReplaceText:= frmReplace.cboReplaceText.Text;
+      fOptions:= frmReplace.SearchOptions;
     end;
   end
   else
   begin
-     frmFind.FindAll:= not fSingleFile;
-     frmFind.cboFindText.Text:= fFindText;
-     return:= frmFind.ShowModal;
-     fSingleFile:=not frmFind.FindAll;
+    frmFind.FindAll:= not fSingleFile;
+    frmFind.cboFindText.Text:= fFindText;
+    return:= frmFind.ShowModal;
+    fSingleFile:=not frmFind.FindAll;
     if (return = mrOk) then
     begin
-        fFindText:= frmFind.cboFindText.Text;
-        fReplaceText:= '';
-        fOptions:= frmFind.SearchOptions;
+      fFindText:= frmFind.cboFindText.Text;
+      fReplaceText:= '';
+      fOptions:= frmFind.SearchOptions;
     end;
   end;
+
   if not (return in [mrOk, mrAll]) then
-   result:= FALSE
-  else
-   if fReplace or (not frmFind.FindAll) then
+    result:= FALSE
+  else if fReplace or (not frmFind.FindAll) then
     result:= RunSingleFile
   else
     result:= RunAllFiles;
@@ -150,22 +134,22 @@ function TdevSearchCenter.RunSingleFile: boolean;
 begin
   if not assigned(fEditor) then
   begin
-     result:= FALSE;
-    exit;
+    Result:= False;
+    Exit;
   end
   else if fEditor.Text.SearchReplace(fFindText, fReplaceText, fOptions) = 0 then
-    MessageDlg(format(Lang[ID_MSG_TEXTNOTFOUND], [SearchCenter.FindText]),
-       mtInformation, [mbOk], 0);//MessageDlg('No match for ' + fFindText, mtInformation, [mbOK], 0);
-  result:= TRUE;
+    MessageDlg(Format(Lang[ID_MSG_TEXTNOTFOUND], [SearchCenter.FindText]),
+       mtInformation, [mbOk], 0);
+  Result:= True;
 end;
 
 function TdevSearchCenter.RunAllFiles: boolean;
 begin
   fReplaceText:= DEV_SEARCHLOOP;
   if frmFind.rbProjectFiles.Checked then
-   result:= RunProject
+    Result:= RunProject
   else
-   result:= RunOpenFiles;
+    Result:= RunOpenFiles;
   fSynEdit.ClearAll;
 end;
 
@@ -173,17 +157,14 @@ function TdevSearchCenter.RunProject: boolean;
 var
   idx: integer;
 begin
-  if not assigned(fProject) then
-  begin
-    // another debug message
-    ShowMessage('ERROR: Searching a Project with no project assigned');
-     result:= FALSE;
-    exit;
-  end;
+  //TODO: lowjoel: The assert is here for temporary purposes. The Search dialog
+  //               disables project searching when no projects are around. Just
+  //               make sure.
+  Assert(assigned(fProject));
 
   for idx:= 0 to pred(fProject.Units.Count) do
   begin
-     fCurFile:= fProject.Units[idx].FileName;
+    fCurFile:= fProject.Units[idx].FileName;
     if ExtractFilePath(fCurFile) = '' then
       fCurFile:= ExpandFileto(fCurFile, fProject.Directory);
 
@@ -194,7 +175,7 @@ begin
 
     fSynEdit.SearchReplace(fFindText, fReplaceText, fOptions);
   end;
-  result:= TRUE;
+  result:= True;
 end;
 
 function TdevSearchCenter.RunOpenFiles: boolean;
@@ -203,8 +184,8 @@ var
 begin
   for idx:= 0 to pred(fPC.PageCount) do
   begin
-     fCurFile:= TEditor(fPC.Pages[idx].Tag).FileName;
-     fSynEdit.Lines:= TEditor(fPC.Pages[idx].Tag).Text.Lines;
+    fCurFile:= TEditor(fPC.Pages[idx].Tag).FileName;
+    fSynEdit.Lines:= TEditor(fPC.Pages[idx].Tag).Text.Lines;
     fSynEdit.SearchReplace(fFindText, fReplaceText, fOptions);
   end;
   result:= TRUE;
@@ -218,12 +199,12 @@ var
 begin
   if fReplaceText = DEV_SEARCHLOOP then
   begin
-     SR.pt:= point(Line, Column);
-     SR.InFile:= fCurFile;
-     SR.msg:= fSynEdit.Lines[Line -1];
+    SR.pt:= point(Line, Column);
+    SR.InFile:= fCurFile;
+    SR.msg:= fSynEdit.Lines[Line -1];
     fSearchProc(SR);
   end;
-  action:= raSkip;
+  Action:= raSkip;
 end;
 
 constructor TdevSearchCenter.Create;
@@ -246,5 +227,6 @@ end;
 initialization
  SearchCenter:= TdevSearchCenter.Create;
 finalization
-  SearchCenter.Free;
+ SearchCenter.Free;
+
 end.
