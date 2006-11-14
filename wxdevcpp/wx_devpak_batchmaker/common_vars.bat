e59f1bc93baa85d98f8089fa8f22ca19b2a364d8
tbreina <tbreina@917957a4-63a1-4874-a357-9ccf276ff571>
2006-11-14 19:24:32 +0000

    Updated the wx batch devpak maker. The lib maker now creates subdirectories for the wxWidgets lib and common devpaks.
    git-svn-id: svn://svn.code.sf.net/p/wxdsgn/code/trunk@673 917957a4-63a1-4874-a357-9ccf276ff571

--- wxdevcpp/wx_devpak_batchmaker/common_vars.bat
+++ wxdevcpp/wx_devpak_batchmaker/common_vars.bat
@@ -1,5 +1,23 @@
 rem This contains all of the common variables used in all batch files
 
-set WXWIN=c:\wxWidgets-2.6.3
+echo off
+cls
+
+rem Default wxWidgets root directory
+set WXVER=2.7.2
+
+echo What version of wxWidgets are you building (Default = %WXVER%)?
+set /P WXVER=
+
+set WXWIN=\wxMSW-%WXVER%
+
+echo In what directory is wxWidgets located (Default = %WXWIN%)?
+set /P WXWIN=
+
+
+rem The path needs to point to where your mingw32-make executable lives
 path=c:\Progra~1\Dev-Cpp\bin;%path%
+
+echo on
+
 
--- wxdevcpp/wx_devpak_batchmaker/wxWidgets.bat
+++ wxdevcpp/wx_devpak_batchmaker/wxWidgets.bat
@@ -1,28 +1,66 @@
 CALL common_vars.bat
 
+set STARTDIR=%CD%
+
 cd %WXWIN%
 
 rem build the main distribution of wxWidgets
 cd build\msw
+
+rem Clean the wxWidgets directories
+mingw32-make -f makefile.gcc clean
+
+rem Build the wxWidgets libraries
 mingw32-make -f makefile.gcc BUILD=release MONOLITHIC=1 USE_OPENGL=1 USE_XRC=1 USE_ODBC=1 RUNTIME_LIBS=static USE_RTTI=0 USE_EXCEPTIONS=1 2> error_base.txt
 
-cd ..\..
+
+cd %WXWIN%
 
 rem make the folder for contrib libraries
 md contrib\lib\gcc_lib
 
 rem replace all occurrences of ..\..\..\lib\gcc_ with ..\..\lib\gcc_ in contrib makefiles
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\deprecated\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\fl\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\foldbar\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\gizmos\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\ifm\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\mmedia\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\net\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\ogl\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\plot\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\stc\makefile.gcc
-C:\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\svg\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\deprecated\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\fl\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\foldbar\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\gizmos\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\ifm\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\mmedia\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\net\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\ogl\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\plot\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\stc\makefile.gcc
+%STARTDIR%\gsar -s..\..\..\lib\gcc_ -r..\..\lib\gcc_ -o contrib\build\svg\makefile.gcc
+
+rem Copy the setup.h up one level
+cd %WXWIN%\include\wx\msw
+copy setup.h ..\setup.h
+
+
+rem Write the files to subdirectory setup devpak
+
+cd %STARTDIR%
+
+set DEVPAKDIR=devpaks_%WXVER%
+md %DEVPAKDIR%
+
+copy wxWidgets_common.DevPackage %DEVPAKDIR%\wxWidgets_%WXVER%_common.DevPackage
+copy wxWidgets_gcc.DevPackage %DEVPAKDIR%\wxWidgets_%WXVER%_gcc.DevPackage
+
+cd %DEVPAKDIR%
+
+%STARTDIR%\gsar -s_WXVER_ -r"%WXVER%" -o wxWidgets_%WXVER%_common.DevPackage
+%STARTDIR%\gsar -s_WXVER_ -r"%WXVER%" -o wxWidgets_%WXVER%_gcc.DevPackage
+
+%STARTDIR%\gsar -s_WXWIN_ -r"%WXWIN%" -o wxWidgets_%WXVER%_common.DevPackage
+%STARTDIR%\gsar -s_WXWIN_ -r"%WXWIN%" -o wxWidgets_%WXVER%_gcc.DevPackage
+
+rem Copy the wxWidgets libs and include files to the new devpak directory
+xcopy /S %WXWIN%\lib\gcc_lib gcc_lib\
+xcopy /S %WXWIN%\include include\
+
+
+cd %WXWIN%
 
 cd contrib\build\deprecated
 mingw32-make -f makefile.gcc BUILD=release MONOLITHIC=1 USE_OPENGL=1 USE_XRC=1 USE_ODBC=1 RUNTIME_LIBS=static USE_RTTI=0 USE_EXCEPTIONS=1 2> error_deprecated.txt 
@@ -60,12 +98,12 @@ mingw32-make -f makefile.gcc BUILD=release MONOLITHIC=1 USE_OPENGL=1 USE_XRC=1 U
 cd ..\..\..
 
 rem Change the plotctrl files 
-C:\gsar -sWX_DEFINE_USER_EXPORTED_ARRAY_DOUBLE -r//WX_DEFINE_USER_EXPORTED_ARRAY_DOUBLE -o 3rdparty\include/wx/plotctrl/plotcurv.h
+gsar -sWX_DEFINE_USER_EXPORTED_ARRAY_DOUBLE -r//WX_DEFINE_USER_EXPORTED_ARRAY_DOUBLE -o 3rdparty\include/wx/plotctrl/plotcurv.h
 rem make the folder for 3rd party libraries
 md 3rdparty\lib\gcc_lib
 
 rem Change the sheet files     
-rem C:\gsar -s#include:x20"wx/dcscreen.h":x0d:x0a#endif -r#include "wx/dcscreen.h":x0d:x0a#include:x20"wx/combobox.h":x0d:x0a#endif -o 3rdParty\src\sheet\sheet.cpp
+rem gsar -s#include:x20"wx/dcscreen.h":x0d:x0a#endif -r#include "wx/dcscreen.h":x0d:x0a#include:x20"wx/combobox.h":x0d:x0a#endif -o 3rdParty\src\sheet\sheet.cpp
 
 
 cd 3rdparty\build\chartart
@@ -96,5 +134,7 @@ mingw32-make -f makefile.gcc BUILD=release MONOLITHIC=1 USE_OPENGL=1 USE_XRC=1 U
 cd ..\treemultictrl
 mingw32-make -f makefile.gcc BUILD=release MONOLITHIC=1 USE_OPENGL=1 USE_XRC=1 USE_ODBC=1 RUNTIME_LIBS=static USE_RTTI=0 USE_EXCEPTIONS=1 2> error_treemultictrl.txt 
 
+rem Change back to the initial directory
+cd %STARTDIR%
 
 pause
