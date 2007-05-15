36e87f4b39d964dacd99ac554a6d2bf86695b16a
gururamnath <gururamnath@917957a4-63a1-4874-a357-9ccf276ff571>
2007-05-15 03:47:39 +0000

    Updated the Copyright Information
    git-svn-id: svn://svn.code.sf.net/p/wxdsgn/code/trunk@936 917957a4-63a1-4874-a357-9ccf276ff571

--- wxdevcpp/source/components/wxHtmlEasyPrinting.pas
+++ wxdevcpp/source/components/wxHtmlEasyPrinting.pas
@@ -1,4 +1,27 @@
 // $Id$
+{                                                                    }
+{   Copyright © 2003-2007 by Guru Kathiresan                         }
+{                                                                    }
+{License :                                                           }
+{=========                                                           }
+{The wx-devC++ Components, Form Designer, Utils classes              }
+{are exclusive properties of Guru Kathiresan.                        }
+{The code is available in dual Licenses:                             }
+{                               1)GPL Compatible  License            }
+{                               2)Commercial License                 }
+{                                                                    }
+{1)GPL License :                                                     }
+{ Code can be used in any project as long as the project's sourcecode}
+{ is published under GPL license.                                    }
+{                                                                    }
+{2)Commercial License:                                               }
+{Use of code in this file or the one that bear this license text     }
+{can be used in Non-GPL projects as long as you get the permission   }
+{from the Author - Guru Kathiresan.                                  }
+{Use of the Code in any non-gpl projects without the permission of   }
+{the author is illegal.                                              }
+{Contact gururamnath@yahoo.com for details                           }
+{ ****************************************************************** }
 
 
 unit wxHtmlEasyPrinting;
